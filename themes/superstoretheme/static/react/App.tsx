f488f3f894e7a65b7c8843ec8b3bd370b5d626a5
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-08-03 13:41:31 +0200

    Configured production build

--- themes/superstoretheme/static/react/App.tsx
+++ themes/superstoretheme/static/react/App.tsx
@@ -11,23 +11,23 @@ import ViewProduct from './views/products/ViewProduct'
 function App() {
   return (
     <div className="App mx-auto max-w-6xl text-center my-8">
-      <h1 className="font-semibold text-2xl">React - The Road To Enterprise</h1>
+      <h1 className="font-semibold text-2xl">Enterprise React</h1>
       <div className="mt-8 ">
         <BrowserRouter>
           <nav className="space-x-4 mb-6">
-            <Link to="/react-essentials/products">Browse Products</Link>
+            <Link to="/react-essentials">Browse Products</Link>
             <Link to="/react-essentials/products/add">Add Product</Link>
             <Link to="/react-essentials/products/1/edit">Edit Product</Link>
             <Link to="/react-essentials/products/1/delete">Delete Product</Link>
             <Link to="/react-essentials/products/1">View Product</Link>
           </nav>
           <Routes>
-            <Route path="/react-essentials/products">
+            <Route path="/react-essentials">
               <Route index element={<Products />} />
-              <Route path="add" element={<AddProduct />} />
-              <Route path=":id/edit" element={<EditProduct />} />
-              <Route path=":id/delete" element={<DeleteProduct />} />
-              <Route path=":id" element={<ViewProduct />} />
+              <Route path="products/add" element={<AddProduct />} />
+              <Route path="products/:id/edit" element={<EditProduct />} />
+              <Route path="products/:id/delete" element={<DeleteProduct />} />
+              <Route path="products/:id" element={<ViewProduct />} />
             </Route>
           </Routes>
         </BrowserRouter>
